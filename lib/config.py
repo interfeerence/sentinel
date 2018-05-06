19f3abd151013dcf7dcdfa915057fb2a33b44f44
interfeerence <36547842+interfeerence@users.noreply.github.com>
2018-05-06 13:20:37 -0300

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -3,27 +3,27 @@
 """
 import sys
 import os
-from omegacoin_config import OmegaConfig
+from exilium_config import ExiliumConfig
 
 default_sentinel_config = os.path.normpath(
     os.path.join(os.path.dirname(__file__), '../sentinel.conf')
 )
 sentinel_config_file = os.environ.get('SENTINEL_CONFIG', default_sentinel_config)
-sentinel_cfg = OmegaConfig.tokenize(sentinel_config_file)
+sentinel_cfg = ExiliumConfig.tokenize(sentinel_config_file)
 sentinel_version = "1.1.0"
-min_omegacoind_proto_version_with_sentinel_ping = 70207
+min_exiliumd_proto_version_with_sentinel_ping = 70207
 
 
-def get_omegacoin_conf():
+def get_exilium_conf():
     home = os.environ.get('HOME')
 
-    omegacoin_conf = os.path.join(home, ".omegacoincore/omegacoin.conf")
+    exilium_conf = os.path.join(home, ".exilium/exilium.conf")
     if sys.platform == 'darwin':
-        omegacoin_conf = os.path.join(home, "Library/Application Support/OmegaCoinCore/omegacoin.conf")
+        exilium_conf = os.path.join(home, "Library/Application Support/Exilium/exilium.conf")
 
-    omegacoin_conf = sentinel_cfg.get('omegacoin_conf', omegacoin_conf)
+    exilium_conf = sentinel_cfg.get('exilium_conf', exilium_conf)
 
-    return omegacoin_conf
+    return exilium_conf
 
 
 def get_network():
@@ -79,6 +79,6 @@ def get_db_conn():
     return db
 
 
-omegacoin_conf = get_omegacoin_conf()
+exilium_conf = get_exilium_conf()
 network = get_network()
 db = get_db_conn()
