62de70ebe1c1f1cd8fe35a72e812362824d2d1ff
interfeerence <36547842+interfeerence@users.noreply.github.com>
2018-05-06 14:15:40 -0300

    Update test_jsonrpc.py

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -8,30 +8,30 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import config
 
-from omegacoind import OmegaDaemon
-from omegacoin_config import OmegaConfig
+from exiliumd import ExiliumDaemon
+from exilium_config import ExiliumConfig
 
 
-def test_omegacoind():
-    config_text = OmegaConfig.slurp_config_file(config.omegacoin_conf)
+def test_exiliumd():
+    config_text = ExiliumConfig.slurp_config_file(config.exilium_conf)
     network = 'mainnet'
     is_testnet = False
-    genesis_hash = u'0000041a9498884aa2506b9dc832af53de1cbee3dca196a8d7b4319ee3a0f27c'
+    genesis_hash = u'00000254161b06b5477d23be9c9098a3938682e1526032ff70a3ee112312ebfc'
     for line in config_text.split("\n"):
         if line.startswith('testnet=1'):
             network = 'testnet'
             is_testnet = True
             genesis_hash = u'00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c'
 
-    creds = OmegaConfig.get_rpc_creds(config_text, network)
-    omegacoind = OmegaDaemon(**creds)
-    assert omegacoind.rpc_command is not None
+    creds = ExiliumConfig.get_rpc_creds(config_text, network)
+    exiliumd = ExiliumDaemon(**creds)
+    assert exiliumd.rpc_command is not None
 
-    assert hasattr(omegacoind, 'rpc_connection')
+    assert hasattr(exiliumd, 'rpc_connection')
 
     # Omega testnet block 0 hash == 0000041a9498884aa2506b9dc832af53de1cbee3dca196a8d7b4319ee3a0f27c
     # test commands without arguments
-    info = omegacoind.rpc_command('getinfo')
+    info = exiliumd.rpc_command('getinfo')
     info_keys = [
         'blocks',
         'connections',
@@ -48,4 +48,4 @@ def test_omegacoind():
     assert info['testnet'] is is_testnet
 
     # test commands with args
-    assert omegacoind.rpc_command('getblockhash', 0) == genesis_hash
+    assert exiliumd.rpc_command('getblockhash', 0) == genesis_hash
