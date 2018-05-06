3e43d605e26ba08498d754ca6720234ecf60d84e
interfeerence <36547842+interfeerence@users.noreply.github.com>
2018-05-06 13:23:45 -0300

    Update governance_class.py

--- lib/governance_class.py
+++ lib/governance_class.py
@@ -19,26 +19,26 @@ class GovernanceClass(object):
         return self.governance_object
 
     # pass thru to GovernanceObject#vote
-    def vote(self, omegacoind, signal, outcome):
-        return self.go.vote(omegacoind, signal, outcome)
+    def vote(self, exiliumd, signal, outcome):
+        return self.go.vote(exiliumd, signal, outcome)
 
     # pass thru to GovernanceObject#voted_on
     def voted_on(self, **kwargs):
         return self.go.voted_on(**kwargs)
 
-    def vote_validity(self, omegacoind):
+    def vote_validity(self, exiliumd):
         if self.is_valid():
             printdbg("Voting valid! %s: %d" % (self.__class__.__name__, self.id))
-            self.vote(omegacoind, models.VoteSignals.valid, models.VoteOutcomes.yes)
+            self.vote(exiliumd, models.VoteSignals.valid, models.VoteOutcomes.yes)
         else:
             printdbg("Voting INVALID! %s: %d" % (self.__class__.__name__, self.id))
-            self.vote(omegacoind, models.VoteSignals.valid, models.VoteOutcomes.no)
+            self.vote(exiliumd, models.VoteSignals.valid, models.VoteOutcomes.no)
 
     def get_submit_command(self):
         object_fee_tx = self.go.object_fee_tx
 
-        import omegacoinlib
-        obj_data = omegacoinlib.SHIM_serialise_for_omegacoind(self.serialise())
+        import exiliumlib
+        obj_data = exiliumlib.SHIM_serialise_for_exiliumd(self.serialise())
 
         cmd = ['gobject', 'submit', '0', '1', str(int(time.time())), obj_data, object_fee_tx]
 
@@ -59,8 +59,8 @@ class GovernanceClass(object):
         return {self.object_hash: dikt}
 
     def get_submit_command(self):
-        import omegacoinlib
-        obj_data = omegacoinlib.SHIM_serialise_for_omegacoind(self.serialise())
+        import exiliumlib
+        obj_data = exiliumlib.SHIM_serialise_for_exiliumd(self.serialise())
 
         # new objects won't have parent_hash, revision, etc...
         cmd = ['gobject', 'submit', '0', '1', str(int(time.time())), obj_data]
@@ -71,15 +71,15 @@ class GovernanceClass(object):
 
         return cmd
 
-    def submit(self, omegacoind):
+    def submit(self, exiliumd):
         # don't attempt to submit a superblock unless a masternode
         # note: will probably re-factor this, this has code smell
-        if (self.only_masternode_can_submit and not omegacoind.is_masternode()):
+        if (self.only_masternode_can_submit and not exiliumd.is_masternode()):
             print("Not a masternode. Only masternodes may submit these objects")
             return
 
         try:
-            object_hash = omegacoind.rpc_command(*self.get_submit_command())
+            object_hash = exiliumd.rpc_command(*self.get_submit_command())
             printdbg("Submitted: [%s]" % object_hash)
         except JSONRPCException as e:
             print("Unable to submit: %s" % e.message)
@@ -95,9 +95,9 @@ class GovernanceClass(object):
 
         return binascii.hexlify(simplejson.dumps((obj_type, self.get_dict()), sort_keys=True).encode('utf-8')).decode('utf-8')
 
-    def omegacoind_serialise(self):
-        import omegacoinlib
-        return omegacoinlib.SHIM_serialise_for_omegacoind(self.serialise())
+    def exiliumd_serialise(self):
+        import exiliumlib
+        return exiliumlib.SHIM_serialise_for_exiliumd(self.serialise())
 
     @classmethod
     def serialisable_fields(self):
