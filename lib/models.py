116f2bdee7830fea3c04ba0dabdd851335ab4cc5
interfeerence <36547842+interfeerence@users.noreply.github.com>
2018-05-06 13:40:14 -0300

    Update models.py

--- lib/models.py
+++ lib/models.py
@@ -12,7 +12,7 @@ from peewee import IntegerField, CharField, TextField, ForeignKeyField, DecimalF
 import peewee
 import playhouse.signals
 import misc
-import omegacoind
+import exiliumd
 from misc import (printdbg, is_numeric)
 import config
 from bitcoinrpc.authproxy import JSONRPCException
@@ -74,8 +74,8 @@ class GovernanceObject(BaseModel):
 
     # sync omegacoind gobject list with our local relational DB backend
     @classmethod
-    def sync(self, omegacoind):
-        golist = omegacoind.rpc_command('gobject', 'list')
+    def sync(self, exiliumd):
+        golist = exiliumd.rpc_command('gobject', 'list')
 
         # objects which are removed from the network should be removed from the DB
         try:
@@ -84,7 +84,7 @@ class GovernanceObject(BaseModel):
                 purged.delete_instance(recursive=True, delete_nullable=True)
 
             for item in golist.values():
-                (go, subobj) = self.import_gobject_from_omegacoind(omegacoind, item)
+                (go, subobj) = self.import_gobject_from_exiliumd(exiliumd, item)
         except Exception as e:
             printdbg("Got an error upon import: %s" % e)
 
@@ -96,9 +96,9 @@ class GovernanceObject(BaseModel):
         return query
 
     @classmethod
-    def import_gobject_from_omegacoind(self, omegacoind, rec):
+    def import_gobject_from_omegacoind(self, exiliumd, rec):
         import decimal
-        import omegacoinlib
+        import exiliumlib
         import inflection
 
         object_hex = rec['DataHex']
@@ -114,8 +114,8 @@ class GovernanceObject(BaseModel):
         }
 
         # shim/omegacoind conversion
-        object_hex = omegacoinlib.SHIM_deserialise_from_omegacoind(object_hex)
-        objects = omegacoinlib.deserialise(object_hex)
+        object_hex = exiliumlib.SHIM_deserialise_from_exiliumd(object_hex)
+        objects = exiliumlib.deserialise(object_hex)
         subobj = None
 
         obj_type, dikt = objects[0:2:1]
@@ -143,14 +143,14 @@ class GovernanceObject(BaseModel):
             newdikt = subdikt.copy()
             newdikt['object_hash'] = object_hash
             if subclass(**newdikt).is_valid() is False:
-                govobj.vote_delete(omegacoind)
+                govobj.vote_delete(exiliumd)
                 return (govobj, None)
 
             subobj, created = subclass.get_or_create(object_hash=object_hash, defaults=subdikt)
         except Exception as e:
             # in this case, vote as delete, and log the vote in the DB
-            printdbg("Got invalid object from omegacoind! %s" % e)
-            govobj.vote_delete(omegacoind)
+            printdbg("Got invalid object from exiliumd! %s" % e)
+            govobj.vote_delete(exiliumd)
             return (govobj, None)
 
         if created:
@@ -172,8 +172,8 @@ class GovernanceObject(BaseModel):
                signal.name, outcome.name]
         return cmd
 
-    def vote(self, omegacoind, signal, outcome):
-        import omegacoinlib
+    def vote(self, exiliumd, signal, outcome):
+        import exiliumlib
 
         # At this point, will probably never reach here. But doesn't hurt to
         # have an extra check just in case objects get out of sync (people will
@@ -203,10 +203,10 @@ class GovernanceObject(BaseModel):
 
         vote_command = self.get_vote_command(signal, outcome)
         printdbg(' '.join(vote_command))
-        output = omegacoind.rpc_command(*vote_command)
+        output = exiliumd.rpc_command(*vote_command)
 
         # extract vote output parsing to external lib
-        voted = omegacoinlib.did_we_vote(output)
+        voted = exiliumlib.did_we_vote(output)
 
         if voted:
             printdbg('VOTE success, saving Vote object to database')
@@ -214,11 +214,11 @@ class GovernanceObject(BaseModel):
                  object_hash=self.object_hash).save()
         else:
             printdbg('VOTE failed, trying to sync with network vote')
-            self.sync_network_vote(omegacoind, signal)
+            self.sync_network_vote(exiliumd, signal)
 
-    def sync_network_vote(self, omegacoind, signal):
+    def sync_network_vote(self, exiliumd, signal):
         printdbg('\tsyncing network vote for object %s with signal %s' % (self.object_hash, signal.name))
-        vote_info = omegacoind.get_my_gobject_votes(self.object_hash)
+        vote_info = exiliumd.get_my_gobject_votes(self.object_hash)
         for vdikt in vote_info:
             if vdikt['signal'] != signal.name:
                 continue
@@ -274,7 +274,7 @@ class Proposal(GovernanceClass, BaseModel):
         db_table = 'proposals'
 
     def is_valid(self):
-        import omegacoinlib
+        import exiliumlib
 
         printdbg("In Proposal#is_valid, for Proposal: %s" % self.__dict__)
 
@@ -305,8 +305,8 @@ class Proposal(GovernanceClass, BaseModel):
                 return False
 
             # payment address is valid base58 omegacoin addr, non-multisig
-            if not omegacoinlib.is_valid_omegacoin_address(self.payment_address, config.network):
-                printdbg("\tPayment address [%s] not a valid Omega address for network [%s], returning False" % (self.payment_address, config.network))
+            if not exiliumlib.is_valid_exilium_address(self.payment_address, config.network):
+                printdbg("\tPayment address [%s] not a valid exilium address for network [%s], returning False" % (self.payment_address, config.network))
                 return False
 
             # URL
@@ -329,7 +329,7 @@ class Proposal(GovernanceClass, BaseModel):
 
     def is_expired(self, superblockcycle=None):
         from constants import SUPERBLOCK_FUDGE_WINDOW
-        import omegacoinlib
+        import exiliumlib
 
         if not superblockcycle:
             raise Exception("Required field superblockcycle missing.")
@@ -341,7 +341,7 @@ class Proposal(GovernanceClass, BaseModel):
         # half the SB cycle, converted to seconds
         # add the fudge_window in seconds, defined elsewhere in Sentinel
         expiration_window_seconds = int(
-            (omegacoinlib.blocks_to_seconds(superblockcycle) / 2) +
+            (exiliumlib.blocks_to_seconds(superblockcycle) / 2) +
             SUPERBLOCK_FUDGE_WINDOW
         )
         printdbg("\texpiration_window_seconds = %s" % expiration_window_seconds)
@@ -409,17 +409,17 @@ class Proposal(GovernanceClass, BaseModel):
             return rank
 
     def get_prepare_command(self):
-        import omegacoinlib
-        obj_data = omegacoinlib.SHIM_serialise_for_omegacoind(self.serialise())
+        import exiliumlib
+        obj_data = exiliumlib.SHIM_serialise_for_exiliumd(self.serialise())
 
         # new superblocks won't have parent_hash, revision, etc...
         cmd = ['gobject', 'prepare', '0', '1', str(int(time.time())), obj_data]
 
         return cmd
 
-    def prepare(self, omegacoind):
+    def prepare(self, exiliumd):
         try:
-            object_hash = omegacoind.rpc_command(*self.get_prepare_command())
+            object_hash = exiliumd.rpc_command(*self.get_prepare_command())
             printdbg("Submitted: [%s]" % object_hash)
             self.go.object_fee_tx = object_hash
             self.go.save()
@@ -447,7 +447,7 @@ class Superblock(BaseModel, GovernanceClass):
         db_table = 'superblocks'
 
     def is_valid(self):
-        import omegacoinlib
+        import exiliumlib
         import decimal
 
         printdbg("In Superblock#is_valid, for SB: %s" % self.__dict__)
@@ -455,7 +455,7 @@ class Superblock(BaseModel, GovernanceClass):
         # it's a string from the DB...
         addresses = self.payment_addresses.split('|')
         for addr in addresses:
-            if not omegacoinlib.is_valid_omegacoin_address(addr, config.network):
+            if not exiliumlib.is_valid_exilium_address(addr, config.network):
                 printdbg("\tInvalid address [%s], returning False" % addr)
                 return False
 
@@ -493,8 +493,8 @@ class Superblock(BaseModel, GovernanceClass):
         pass
 
     def hash(self):
-        import omegacoinlib
-        return omegacoinlib.hashit(self.serialise())
+        import exiliumlib
+        return exiliumlib.hashit(self.serialise())
 
     def hex_hash(self):
         return "%x" % self.hash()
@@ -604,33 +604,33 @@ class Watchdog(BaseModel, GovernanceClass):
     only_masternode_can_submit = True
 
     @classmethod
-    def active(self, omegacoind):
+    def active(self, exiliumd):
         now = int(time.time())
         resultset = self.select().where(
-            self.created_at >= (now - omegacoind.SENTINEL_WATCHDOG_MAX_SECONDS)
+            self.created_at >= (now - exiliumd.SENTINEL_WATCHDOG_MAX_SECONDS)
         )
         return resultset
 
     @classmethod
-    def expired(self, omegacoind):
+    def expired(self, exiliumd):
         now = int(time.time())
         resultset = self.select().where(
-            self.created_at < (now - omegacoind.SENTINEL_WATCHDOG_MAX_SECONDS)
+            self.created_at < (now - exiliumd.SENTINEL_WATCHDOG_MAX_SECONDS)
         )
         return resultset
 
-    def is_expired(self, omegacoind):
+    def is_expired(self, exiliumd):
         now = int(time.time())
         return (self.created_at < (now - omegacoind.SENTINEL_WATCHDOG_MAX_SECONDS))
 
-    def is_valid(self, omegacoind):
-        if self.is_expired(omegacoind):
+    def is_valid(self, exiliumd):
+        if self.is_expired(exiliumd):
             return False
 
         return True
 
-    def is_deletable(self, omegacoind):
-        if self.is_expired(omegacoind):
+    def is_deletable(self, exiliumd):
+        if self.is_expired(exiliumd):
             return True
 
         return False
